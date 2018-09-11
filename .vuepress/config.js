d9fb997f1b8423e502dabbb6272706ceef68e015
zhangxiaoshang <zhangxiaoshang66@163.com>
2018-09-11 20:00:17 +0800

    [完成] Home页翻译

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -29,7 +29,7 @@ var sidebar = [
       ['/wiki/Customization-Overview', 'Customization Overview'],
       ['/wiki/Widget-Constructor', 'a. Widget Constructor'],
       ['/wiki/Widget-Methods', 'b. Widget Methods'],
-      ['wiki/Chart-Methods', 'c. Chart Methods'],
+      ['/wiki/Chart-Methods', 'c. Chart Methods'],
       ['/wiki/Featuresets', 'd. Featuresets'],
       ['/wiki/Customization-Overview#customization-done-through-data-stream', 'Server Side Customization'],
       ['/wiki/Customization-Use-Cases', 'Customization Use Cases']
@@ -51,16 +51,78 @@ var sidebar = [
   }
 ]
 
+var sidebar_zh = [
+  ['/zh/wiki/Home', 'What is Charting Library'],
+  {
+    title: 'Getting Started',
+    collapsable: false,
+    children: [
+      ['/zh/wiki/Package-Content', 'Package Content'],
+      ['/zh/wiki/Running-Your-Charting-Library', 'Running Your Charting Library'],
+      ['/zh/wiki/Best-Practices', 'Best Practices'],
+      ['/zh/wiki/Frequently-Asked-Questions', 'Frequently Asked Questions']
+    ]
+  },
+  {
+    title: 'Data Binding',
+    collapsable: false,
+    children: [
+      ['/zh/wiki/How-To-Connect-My-Data', 'How To Connect My Data'],
+      ['/zh/wiki/JS-Api', 'JS Api'],
+      ['/zh/wiki/UDF', 'UDF'],
+      ['/zh/wiki/Symbology', 'Symbology'],
+      ['/zh/wiki/Trading-Sessions', 'Trading Sessions'],
+      ['/zh/wiki/Quotes', 'Quotes']
+    ]
+  },
+  {
+    title: 'Chart Customization',
+    collapsable: false,
+    children: [
+      ['/zh/wiki/Customization-Overview', 'Customization Overview'],
+      ['/zh/wiki/Widget-Constructor', 'a. Widget Constructor'],
+      ['/zh/wiki/Widget-Methods', 'b. Widget Methods'],
+      ['/zh/wiki/Chart-Methods', 'c. Chart Methods'],
+      ['/zh/wiki/Featuresets', 'd. Featuresets'],
+      ['/zh/wiki/Customization-Overview#customization-done-through-data-stream', 'Server Side Customization'],
+      ['/zh/wiki/Customization-Use-Cases', 'Customization Use Cases']
+    ]
+  },
+  {
+    title: 'Trading Terminal',
+    collapsable: false,
+    children: [
+      ['/wiki/Trading-Terminal', '1. Brief Intro'],
+      ['/wiki/Broker-API', '2. Broker API'],
+      ['/wiki/Trading-Host', '3. Trading Host'],
+      ['/wiki/Account-Manager', '4. Account Manager'],
+      ['/wiki/Trading-Objects-and-Constants', ' 5.Trading Objects and Constants'],
+      ['/wiki/Saving-and-Loading-Charts', 'Saving/Loading Charts'],
+      ['/wiki/Creating-Custom-Studies', 'Creating Custom Studies'],
+      ['/wiki/Breaking-Changes', 'Breaking Changes In Version']
+    ]
+  }
+]
+
 module.exports = {
   base: '/charting_library_wiki/',
+  configureWebpack: {
+    resolve: {
+      alias: {
+        '@zh': 'zh/wiki'
+      }
+    }
+  },
   locales: {
     '/': {
       lang: 'en-US',
-      title: 'Charting Library'
+      title: 'Charting Library',
+      description: 'Charting Library Wiki  TradingView Docs'
     },
     '/zh/': {
       lang: 'zh-CN',
-      title: 'Charting Library'
+      title: 'Charting Library',
+      description: 'TradingView 中文文档 教程'
     }
   },
   themeConfig: {
@@ -75,7 +137,7 @@ module.exports = {
         label: '简体中文',
         selectText: '选择语言',
         lastUpdated: '上次更新',
-        sidebar: sidebar
+        sidebar: sidebar_zh
       }
     }
   }
--- zh/wiki/Home.md
+++ zh/wiki/Home.md
@@ -1,25 +1,25 @@
 ### Charting Library
 
-Charting library is a standalone solution that you download, host on your servers, connect your own data & use in your site/app for free.
+你下载的Charting library是一个独立的解决方案，可以部署在自己的服务器上，连接自己的数据，并在自己的站点/app中使用，这一切都是免费的。
 
-| What to do | What you'll get after |
+| 做什么 | 可以得到什么 |
 |---|---|
-|1. `git clone` your [copy of Charting Library](Package-Content) and then [run it](Running-Your-Charting-Library)|A working example of our chart running on your host|
-|2. Plug your data into Charting Library using [one of our APIs](How-To-Connect-My-Data). You can see some examples [here](How-To-Connect-My-Data#examples)|The complete and working charting tool with your own data|
+|1. `git clone` 你的 [图表库副本](Package-Content.md) 然后 [运行](Running-Your-Charting-Library.md)|我们的图表在你的主机上运行的一个示例|
+|2. 使用我们的[API](How-To-Connect-My-Data.md)将你的数据插入Charting Library. 你可以在[这里](How-To-Connect-My-Data.md#examples)查看一些示例 |包含你自己的数据，完整且有效的图表工具|
 
-If you want to customize your charts, then you may go further.
+如果你想自定义图表，那么你可以更进一步。
 
-* Read Charting Library [Customization Overview](Customization-Overview) and [Customization Use Cases](Customization-Use-Cases)
-* [Creating Custom Studies](Creating-Custom-Studies)
+* 阅读Charting Library [Customization Overview](Customization-Overview.md) 和 [Customization Use Cases](Customization-Use-Cases.md)
+* [Creating Custom Studies](Creating-Custom-Studies.md)
 
 ### Examples
 
-We’ve got a [public GitHub repository](https://github.com/tradingview/charting-library-examples) that includes examples of Charting Library integration. The examples illustrate how it can be integrated with different Web Frameworks.
+我们有一个 [public GitHub repository](https://github.com/tradingview/charting-library-examples) 其中包含图表库集成的示例. 这些示例说明了如何将其与不同的Web框架集成。
 
 ### Trading Terminal
 
-:chart: Trading Terminal is a ready-to-use product for those who want to have a great charting solution along with the ability to trade right from the chart. [Read more](Trading-Terminal).
+:chart: Trading Terminal 是一款随时可用的产品，适合那些希望拥有出色图表解决方案以及从图表中进行交易的人士。 [阅读更多](Trading-Terminal.md).
 
 ### Best Practices
 
-Reading [this article](Best-Practices) will prevent you from making the most common mistakes and will **save your time**.
+阅读 [这篇文章](Best-Practices.md) 可以避免犯常见的错误并 **节省你的时间**.
